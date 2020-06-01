e861b9f5870edac78081963f41748475d41dafdb
m0skit0 <ametric@gmail.com>
2020-06-01 23:20:21 +0200

    Added pointcut. However AspectJ only seems to inject on method calls made in the source code. We can't capture calls made in the Android framework.

--- testapplication/src/main/java/org/m0skit0/android/mapswrapper/testapp/MethodCallCatcherAspect.kt
+++ testapplication/src/main/java/org/m0skit0/android/mapswrapper/testapp/MethodCallCatcherAspect.kt
@@ -1,12 +1,18 @@
 package org.m0skit0.android.mapswrapper.testapp
 
-//@Aspect
-//class MethodCallCatcherAspect {
-//
-//    private val TAG = "[ASPECTJ]"
-//
-//    @Before("execution(* onCreate(*)) || execution(* onCreate())")
-//    fun anySupportMapFragmentCall(joinPoint: JoinPoint) {
-//        Log.d(TAG, "Method called: ${joinPoint.toLongString()}")
-//    }
-//}
+import android.util.Log
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+
+@Aspect
+class MethodCallCatcherAspect {
+
+    private val TAG = "[ASPECTJ]"
+
+    @Around("call(* org.m0skit0.android.mapswrapper..*(..))")
+    fun anySupportMapFragmentCall(joinPoint: ProceedingJoinPoint) {
+        Log.d(TAG, "Method called: ${joinPoint.toLongString()}")
+        joinPoint.proceed()
+    }
+}
