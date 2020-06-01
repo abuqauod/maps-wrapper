635a8af4e7df7d4b0664d842c49de1ae2acc5739
m0skit0 <ametric@gmail.com>
2020-06-01 22:45:15 +0200

    Fixed ClassNotFoundException: Didn't find class "androidx.appcompat.R$drawable" by using locally generated plugin

--- testapplication/src/main/java/org/m0skit0/android/mapswrapper/testapp/MethodCallCatcherAspect.kt
+++ testapplication/src/main/java/org/m0skit0/android/mapswrapper/testapp/MethodCallCatcherAspect.kt
@@ -1,17 +1,12 @@
 package org.m0skit0.android.mapswrapper.testapp
 
-import android.util.Log
-import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.annotation.Before
-
-@Aspect
-class MethodCallCatcherAspect {
-
-    private val TAG = "[ASPECTJ]"
-
-    @Before("execution(* onCreate(*)) || execution(* onCreate())")
-    fun anySupportMapFragmentCall(joinPoint: JoinPoint) {
-        Log.d(TAG, "Method called: ${joinPoint.toLongString()}")
-    }
-}
+//@Aspect
+//class MethodCallCatcherAspect {
+//
+//    private val TAG = "[ASPECTJ]"
+//
+//    @Before("execution(* onCreate(*)) || execution(* onCreate())")
+//    fun anySupportMapFragmentCall(joinPoint: JoinPoint) {
+//        Log.d(TAG, "Method called: ${joinPoint.toLongString()}")
+//    }
+//}
